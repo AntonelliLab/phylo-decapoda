21709732dd57ef292cd1bf3b82d8334c5b180b23
hettling <hannes.hettling@gmail.com>
2016-10-14 10:50:16 +0200

    counting marker occurrences in rows

--- script/plot-markers.R
+++ script/plot-markers.R
@@ -1,5 +1,8 @@
 tab <- read.table('markers-backbone.tsv', header=T, sep='\t', row.names=1)
 tab <- tab[-ncol(tab)]
+
 tt <- apply(tab, 2, function(x)ifelse(x=='', 0, 1))
+
 heatmap(tt, Rowv=NA, Colv=NA, col=c('white', 'black'), xlab="marker", ylab='species', margins=c(5,8), scale='row')
 
+length(which(colSums(tt) < 3))
